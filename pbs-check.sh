08aee854db468c32e1b578e23ee4d86fa09ce316
Wladimir-N <46901390+Wladimir-N@users.noreply.github.com>
2022-11-01 10:52:54 +0300

    Update pbs-check.sh

--- pbs-check.sh
+++ pbs-check.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 source $(dirname "$(realpath $0)")/.env
 telegramm () {
-	test=
-	while [ "$(echo $test | cut -d '"' -f 2)" != "ok" ]; do
-		test=$(curl --header 'Content-Type: application/json' --request 'POST' --data "{\"chat_id\":\"${chat_id}\",\"parse_mode\":\"HTML\",\"text\":\"$1\"}" "https://api.telegram.org/bot${token}/sendMessage")
-		sleep 1
-	done
+	if [ ! -z ${chat_id} ] && [ ! -z ${token} ]
+	then
+		test=
+		while [ "$(echo $test | cut -d '"' -f 2)" != "ok" ]; do
+			test=$(curl --header 'Content-Type: application/json' --request 'POST' --data "{\"chat_id\":\"${chat_id}\",\"parse_mode\":\"HTML\",\"text\":\"$1\"}" "https://api.telegram.org/bot${token}/sendMessage")
+			sleep 1
+		done
+	fi
 }
 send_email () {
-  if [ ! -z ${email} ]
-  then
-    echo "$2" | mail -s "no backup - $(hostname -f) - $1" ${email}
-  fi
-  if [ ! -z ${email_html} ]
-  then
-    echo "<code>$2</code>" | mail -s "$no backup - (echo -e "$(hostname -f) - $1\nContent-Type: text/html")" ${email_html}
-  fi
+	if [ ! -z ${email} ]
+	then
+		echo "$2" | mail -s "no backup - $(hostname -f) - $1" ${email}
+	fi
+	if [ ! -z ${email_html} ]
+	then
+		echo "<code>$2</code>" | mail -s "$no backup - (echo -e "$(hostname -f) - $1\nContent-Type: text/html")" ${email_html}
+	fi
 }
 while read datastore
 do
