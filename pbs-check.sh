49bda15fa5405a86f6d80ff627a7304d6d8db702
Wladimir-N <46901390+Wladimir-N@users.noreply.github.com>
2022-12-06 11:41:39 +0300

    Update pbs-check.sh

--- pbs-check.sh
+++ pbs-check.sh
@@ -28,8 +28,8 @@ do
 		export check="$(proxmox-backup-client list --ns ${namespace} | grep -vE "($(echo ${DatastoreTypeVMID} | tr '/' '\n' | grep "${datastore}-" | cut -d '-' -f2,3 | tr '-' '/' | tr '\n' '|')$(date +"/%Y-%m-%dT")|$(date -d "1 day ago"  +"/%Y-%m-%dT")|├)")"
 		if [ "$(proxmox-backup-client list --ns ${namespace} | grep -vE "($(date +"/%Y-%m-%dT")|$(date -d "1 day ago"  +"/%Y-%m-%dT")|├)" | grep -E '(ct|vm)/' | wc -l)" -gt "0" ]
 		then
-    			send_email "${datastore}" "${check}"
-			telegramm "$(hostname -f) - $datastore<code>${check}</code>"
+    			send_email "${datastore}/${namespace}" "${check}"
+			telegramm "$(hostname -f) - ${datastore}/${namespace}<code>${check}</code>"
 		fi
 	done < <(proxmox-backup-client namespace list)
 	export check="$(proxmox-backup-client list | grep -vE "($(echo ${DatastoreTypeVMID} | tr '/' '\n' | grep "${datastore}-" | cut -d '-' -f2,3 | tr '-' '/' | tr '\n' '|')$(date +"/%Y-%m-%dT")|$(date -d "1 day ago"  +"/%Y-%m-%dT")|├)")"
