5ca723f7696302332af2486832a4e14d3c6419a1
Wladimir-N <46901390+Wladimir-N@users.noreply.github.com>
2022-12-06 12:14:15 +0300

    Update pbs-check.sh

--- pbs-check.sh
+++ pbs-check.sh
@@ -25,8 +25,8 @@ do
 	export PBS_REPOSITORY=$(hostname -f):${datastore}
 	while read namespace
 	do
-		export check="$(proxmox-backup-client list --ns ${namespace} | grep -vE "($(echo ${DatastoreTypeVMID} | tr '/' '\n' | grep "${datastore}-" | cut -d '-' -f2,3 | tr '-' '/' | tr '\n' '|')$(date +"/%Y-%m-%dT")|$(date -d "1 day ago"  +"/%Y-%m-%dT")|├)")"
-		if [ "$(proxmox-backup-client list --ns ${namespace} | grep -vE "($(date +"/%Y-%m-%dT")|$(date -d "1 day ago"  +"/%Y-%m-%dT")|├)" | grep -E '(ct|vm)/' | wc -l)" -gt "0" ]
+		export check="$(proxmox-backup-client list --ns ${namespace} | grep -vE "($(echo ${DatastoreTypeVMID} | tr '/' '\n' | grep "${datastore}:${namespace}-" | cut -d '-' -f2,3 | tr '-' '/' | tr '\n' '|')$(date +"/%Y-%m-%dT")|$(date -d "1 day ago"  +"/%Y-%m-%dT")|├)")"
+		if [ "$(proxmox-backup-client list --ns ${namespace} | grep -vE "($(echo ${DatastoreTypeVMID} | tr '/' '\n' | grep "${datastore}:${namespace}-" | cut -d '-' -f2,3 | tr '-' '/' | tr '\n' '|')$(date +"/%Y-%m-%dT")|$(date -d "1 day ago"  +"/%Y-%m-%dT")|├)" | grep -E '(ct|vm)/' | wc -l)" -gt "0" ]
 		then
     			send_email "${datastore}/${namespace}" "${check}"
 			telegramm "$(hostname -f) - ${datastore}/${namespace}<code>${check}</code>"
