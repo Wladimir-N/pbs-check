37f78321ef12053b733fd0f3f6ba7d5fecc12bbe
Wladimir-N <46901390+Wladimir-N@users.noreply.github.com>
2023-06-06 15:30:52 +0300

    Добавил определение по датастору, мелкие правки, исправление багов

--- pbs-check.py
+++ pbs-check.py
@@ -12,13 +12,24 @@ load_dotenv()
 N = int(os.environ.get("DAYS"))
 
 # Получение списка пропущенных бэкапов из переменной окружения SKIPPED_BACKUPS
-skipped_backups = os.getenv("SKIPPED_BACKUPS", "").split(",")
+skipped_backups = os.getenv("SKIPPED_BACKUPS")
 
 # Преобразование списка пропущенных бэкапов в словарь
-skipped_backups_dict = {}
-for backup in skipped_backups:
-    backup_id, backup_type = backup.split(":")
-    skipped_backups_dict[backup_id] = backup_type
+if skipped_backups:
+    skipped_backups = skipped_backups.split(",")
+    skipped_backups_dict = {}
+    for backup in skipped_backups:
+        backup_data = backup.split(":")
+        backup_id = backup_data[0]
+        backup_type = backup_data[1]
+        backup_datastore = backup_data[2]
+        if len(backup_data) == 4:
+            backup_namespace = backup_data[3]
+        else:
+            backup_namespace = None
+        skipped_backups_dict[backup_id] = {"type": backup_type, "datastore": backup_datastore, "namespace": backup_namespace}
+else:
+    skipped_backups_dict = {}
 
 # Выполнение команды и получение списка имен datastore'ов
 output = subprocess.check_output("proxmox-backup-manager datastore list --output-format json", shell=True)
@@ -33,6 +44,7 @@ for pbs_repository in pbs_repositories:
     # Получение списка namespace'ов из репозитория
     raw_namespaces = os.popen(f"proxmox-backup-client namespace list --repository {pbs_repository}").read().strip()
     namespaces = raw_namespaces.splitlines()
+    _, datastore = pbs_repository.split(":")
 
     # Если в репозитории есть namespace'ы, то проверяем каждый из них
     if namespaces:
@@ -49,7 +61,7 @@ for pbs_repository in pbs_repositories:
             # Проверка каждого бэкапа
             for backup in backups:
                 # Если backup-id есть в списке пропущенных бэкапов и backup-type совпадает, то пропускаем его
-                if backup["backup-id"] in skipped_backups_dict and skipped_backups_dict[backup["backup-id"]] == backup["backup-type"]:
+                if backup["backup-id"] in skipped_backups_dict and skipped_backups_dict[backup["backup-id"]]["type"] == backup["backup-type"] and skipped_backups_dict[backup["backup-id"]]["datastore"] == datastore and (not skipped_backups_dict[backup["backup-id"]]["namespace"] or skipped_backups_dict[backup["backup-id"]]["namespace"] == namespace):
                     continue
                 # Преобразование времени последнего бэкапа в формат datetime
                 last_backup_time = datetime.fromtimestamp(backup["last-backup"])
@@ -57,7 +69,7 @@ for pbs_repository in pbs_repositories:
                 delta = datetime.now() - last_backup_time
                 # Если разница больше N дней и у машины нет комментария, то добавляем её в список
                 if delta >= timedelta(days=N) and not backup.get("comment"):
-                    machines_without_backups.append(backup["backup-id"])
+                    machines_without_backups.append(backup["backup-type"] + " " + backup["backup-id"])
 
             # Вывод списка машин без бэкапов
             if machines_without_backups:
@@ -80,7 +92,7 @@ for pbs_repository in pbs_repositories:
         # Проверка каждого бэкапа
         for backup in backups:
             # Если backup-id есть в списке пропущенных бэкапов и backup-type совпадает, то пропускаем его
-            if backup["backup-id"] in skipped_backups_dict and skipped_backups_dict[backup["backup-id"]] == backup["backup-type"]:
+            if backup["backup-id"] in skipped_backups_dict and skipped_backups_dict[backup["backup-id"]]["type"] == backup["backup-type"] and skipped_backups_dict[backup["backup-id"]]["datastore"] == datastore and (not skipped_backups_dict[backup["backup-id"]]["namespace"] or skipped_backups_dict[backup["backup-id"]]["namespace"] == namespace):
                 continue
             # Преобразование времени последнего бэкапа в формат datetime
             last_backup_time = datetime.fromtimestamp(backup["last-backup"])
@@ -88,7 +100,7 @@ for pbs_repository in pbs_repositories:
             delta = datetime.now() - last_backup_time
             # Если разница больше N дней и у машины нет комментария, то добавляем её в список
             if delta >= timedelta(days=N) and not backup.get("comment"):
-                machines_without_backups.append(backup["backup-id"])
+                machines_without_backups.append(backup["backup-type"] + " " + backup["backup-id"])
 
         # Вывод списка машин без бэкапов
         if machines_without_backups:
